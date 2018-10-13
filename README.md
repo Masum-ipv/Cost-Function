bfe4d973fefcfbbaa435e0c2bf30f4e563d1da1c
masum-ipv <fakhrulislamcuet@gmail.com>
2018-10-13 14:04:55 +0600

    Add plot image

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # Cost-Function
-Implementation of cost function with gradient descent, Adam optimizer (Python, Tensorflow)
+Implementation of cost function with gradient descent (Python, Tensorflow)
 
 ![alt text](https://cdn-images-1.medium.com/max/800/1*0MUSQabL9Jae_hP2y5C4yg.png)
 
-https://medium.com/@rrfd/what-is-a-cost-function-gradient-descent-examples-with-python-16273460d634
+### Output :
+
+--------------- Gradient Descent with Python -----------------------------
+Iteration 0: X= 25.000000 Y= 35.000000 Z= 3200.000000
+Iteration 100: X= 0.639677 Y= 1.541862 Z= 4.418838
+Iteration 200: X= 0.025448 Y= 0.061438 Z= 0.007013
+Iteration 300: X= 0.001014 Y= 0.002447 Z= 0.000011
+Iteration 400: X= 0.000040 Y= 0.000098 Z= 0.000000
+Iteration 500: X= 0.000002 Y= 0.000004 Z= 0.000000
+Iteration 600: X= 0.000000 Y= 0.000000 Z= 0.000000
+Iteration 700: X= 0.000000 Y= 0.000000 Z= 0.000000
+Iteration 800: X= 0.000000 Y= 0.000000 Z= 0.000000
+Iteration 900: X= 0.000000 Y= 0.000000 Z= 0.000000
+Final Gradient Descent: X= 0.000000 Y= 0.000000 Z= 0.000000
+
+![Plot]()
+
+
+--------------- Gradient Descent with Tensorflow -----------------------------
+Iteration 0: X= 25.000000 Y= 35.000000 Z= 3200.000000
+Iteration 100: X= 0.626844 Y= 1.522830 Z= 4.300604
+Iteration 200: X= 0.025080 Y= 0.060587 Z= 0.006819
+Iteration 300: X= 0.000969 Y= 0.002384 Z= 0.000011
+Iteration 400: X= 0.000038 Y= 0.000094 Z= 0.000000
+Iteration 500: X= 0.000002 Y= 0.000004 Z= 0.000000
+Iteration 600: X= 0.000000 Y= 0.000000 Z= 0.000000
+Iteration 700: X= 0.000000 Y= 0.000000 Z= 0.000000
+Iteration 800: X= 0.000000 Y= 0.000000 Z= 0.000000
+Iteration 900: X= 0.000000 Y= 0.000000 Z= 0.000000
+
+#### Resources:
+* https://medium.com/@rrfd/what-is-a-cost-function-gradient-descent-examples-with-python-16273460d634
--- gradient_descent.py
+++ gradient_descent.py
@@ -1,18 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-#import tensorflow as tf
+import tensorflow as tf
 
 # Create gradient arrays
 grad_x = [] 
 grad_y = []
 grad_z = []
+alpha = .01
+epoch = 1000
+
 def dx (x, y):
     return 8*x - 2*y
 def dy (x, y):
     return 4*y - 2*x
 def func(x, y):
-    return 4*x*x + 2*y*y - 2*x*y + 4
+    return 4*x*x + 2*y*y - 2*x*y
 
 def plot_linear_data(p_x, p_y, p_z):
     fig = plt.figure(figsize=(10, 10))
@@ -36,9 +39,6 @@ def gradient_descent():
     theta_0  = 25
     theta_1  = 35
 
-    alpha = .01
-    epoch = 1000
-
     grad_x.append(theta_0)
     grad_y.append(theta_1)
     grad_z.append(func(theta_0, theta_1))
@@ -46,7 +46,7 @@ def gradient_descent():
     # Run the gradient
     for i in range(epoch):
         if i%100 == 0:
-            print("At: ",i ,theta_0, theta_1, grad_z[-1])
+            print("Iteration %d: X= %f Y= %f Z= %f" % (i ,theta_0, theta_1, grad_z[-1]))
         current_theta_0 = theta_0 - alpha * dx(theta_0, theta_1)
         current_theta_1 = theta_1 - alpha * dy(theta_0, theta_1)
         grad_x.append(current_theta_0)
@@ -59,29 +59,27 @@ def gradient_descent():
         
         # Return last values
     return theta_0, theta_1, grad_z[-1]
-'''
+
 def gradient_descent_tensorflow():    
     x = tf.Variable(25, dtype=tf.float32)
     y = tf.Variable(35, dtype=tf.float32)
 
     cost = 4*x*x + 2*y*y - 2*x*y
-    #cost = x**2 - 10*x + 25
-    train = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
+    train = tf.train.GradientDescentOptimizer(alpha).minimize(cost)
 
     init = tf.global_variables_initializer()
     session = tf.Session()
     session.run(init)
 
-    for i in range(1000):
+    for i in range(epoch):
       if i%100 == 0:
-         print("At: ",i ,session.run(x), session.run(y), session.run(cost))
+         print("Iteration %d: X= %f Y= %f Z= %f" % (i ,session.run(x), session.run(y), session.run(cost)))
       session.run(train)
-      
-    print(session.run(x))
-    print(session.run(y))
-    print(session.run(cost))
-'''
+
+print("\n\n--------------- Gradient Descent with Python -----------------------------")
 theta_0, theta_1, grad_z = gradient_descent()
-print("Final: ", theta_0, theta_1, grad_z)
+print("Final Gradient Descent: X= %f Y= %f Z= %f" % (theta_0, theta_1, grad_z))
 plot_linear_data(grad_x, grad_y, grad_z)
-#gradient_descent_tensorflow()
+
+print("\n\n--------------- Gradient Descent with Tensorflow -----------------------------")
+gradient_descent_tensorflow()
